d91a09b423a78bf3474080f2ca8cdaf56c3db413
jinhankim <saverlip@gmail.com>
2023-08-16 12:55:42 +0900

    update docker

--- run_docker_container.sh
+++ run_docker_container.sh
@@ -1,5 +1,5 @@
 containername="ghrb_framework"
 
 docker ps -aq --filter "name=$containername" | grep -q . && docker stop $containername && docker rm $containername
-docker run -dt --name $containername -v $(pwd)/data:/root/data -v $(pwd)/scripts:/root/scripts -v $(pwd)/results:/root/results -v $(pwd):/root/GHRB jylee/ghrb:latest
+docker run -dt --name $containername -v $(pwd):/root/framework jylee/ghrb_framework:latest
 docker exec -it $containername /bin/bash
